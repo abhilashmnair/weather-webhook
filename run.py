ef3c34b72fbda8289ec5483df58782e6edb78478
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-12-31 11:40:35 +0530

    Final Commit

---
+++ run.py
@@ -0,0 +1,33 @@
+from telegram import *
+from telegram.ext import *
+import requests
+import json
+
+bot = Bot('1472276375:AAHnzbpHcKcSwAtEG7bLvASL6H_dYKCwgWo')
+print(bot.get_me())
+updater = Updater('1472276375:AAHnzbpHcKcSwAtEG7bLvASL6H_dYKCwgWo',use_context=True)
+dispatcher : Dispatcher = updater.dispatcher
+
+def getWeatherData(city_name):
+    key = 'f0f5e3d87f6d51eaef4e8487f94c8bd5'
+    URL = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&units=metric'
+    response = requests.get(url = URL)
+    payload = response.json()
+    try:
+        cur_weather = payload['weather'][0]['main']
+        cur_desc = payload['weather'][0]['description']
+        weather_icon = payload['weather'][0]['icon']
+        cur_temp = payload['main']['temp']
+        return f'Weather in {city_name}\nCurrent weather : {cur_weather}\nTemperature : {cur_temp}°C'
+    except KeyError:
+        return "Sorry, couldn't get the data for given location!"
+
+def sendResponse(update:Update,context:CallbackContext):
+    if update.message.text == '/start':
+        bot.sendMessage(chat_id = update.effective_chat.id, text = 'Enter the city name', parse_mode = 'HTML')
+    else:
+        weatherData = getWeatherData(update.message.text)
+        bot.sendMessage(chat_id = update.effective_chat.id,text = weatherData,parse_mode = 'HTML')
+
+dispatcher.add_handler(MessageHandler(Filters.text,sendResponse))
+updater.start_polling()
